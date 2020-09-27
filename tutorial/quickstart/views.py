fcde591022f6535e77ea4007c7c2c361a6af1a30
mohira <shukatsudaison@gmail.com>
2020-09-27 21:20:51 +0900

    tutorialマトリョーシカ直した

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
 
-from tutorial.tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from tutorial.tutorial.quickstart import views
+from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
