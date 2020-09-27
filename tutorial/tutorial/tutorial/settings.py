72bc91f50618b420bdafae42ed174e20d8abfe79
mohira <shukatsudaison@gmail.com>
2020-09-27 21:02:50 +0900

    サーバー起動直前までできた！

--- tutorial/tutorial/tutorial/settings.py
+++ tutorial/tutorial/tutorial/settings.py
@@ -26,6 +26,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -35,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
