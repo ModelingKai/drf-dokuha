dfd561d32ed09e25425f5dc971f62fc6540d6cbe
Jun Nakajima <ikuta1919@gmail.com>
2020-10-01 23:21:54 +0900

    Tutorial 2終了。 suffixesパターンが強い

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -8,7 +8,7 @@ from .serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
@@ -27,7 +27,7 @@ def snippet_list(request):
 
 
 @csrf_exempt
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
