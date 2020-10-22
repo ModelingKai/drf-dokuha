0b249c9ab6357325d95fa3317626097d0c4f2060
Jun Nakajima <ikuta1919@gmail.com>
2020-10-22 23:09:25 +0900

    引数間違えた

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -31,7 +31,7 @@ class SnippetViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
 
-    @action(detail=True, renderers_classes=[renderers.StaticHTMLRenderer])
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
